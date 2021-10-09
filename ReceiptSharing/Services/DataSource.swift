e1d6496432f99357f5045beae23e858b949f0068
Anton Popkov <a.popkov@opends.tech>
2021-10-09 10:10:18 +0300

    Обновления в DataSource

--- ReceiptSharing/Services/DataSource.swift
+++ ReceiptSharing/Services/DataSource.swift
@@ -5,16 +5,23 @@ final class DataSource {
     private(set) var items: [Item] = []
     private(set) var currentUser: User?
     
+    var onUsersUpdate: (() -> Void)?
+    var onItemsUpdate: (() -> Void)?
+    
     func process(event: Event) {
         switch event {
         case .initial(let initialData):
             users = initialData.users
             items = initialData.items
             currentUser = initialData.currentUser
+            onUsersUpdate?()
+            onItemsUpdate?()
         case .updateItems(let items):
             self.items = items
+            onItemsUpdate?()
         case .updateUsers(let users):
             self.users = users
+            onUsersUpdate?()
         }
     }
 }
