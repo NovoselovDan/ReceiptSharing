6e6f6b7c00afc14c92ed64ca4dbfab39a07404d7
Daniil Novoselov <d.novoselov@opends.tech>
2021-10-09 11:51:08 +0300

    Add layout

--- ReceiptSharing/Screens/ReceiptSplitViewController.swift
+++ ReceiptSharing/Screens/ReceiptSplitViewController.swift
@@ -4,6 +4,11 @@ final class ReceiptSplitViewController: UIViewController {
     private let dataSource: DataSource
     private let commandProcessor: CommandProcessor
     
+    private lazy var collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: creatLayout()
+    )
+    
     init(dataSource: DataSource = .shared,
          commandProcessor: CommandProcessor = FakeCommandProcessor()) {
         self.dataSource = dataSource
@@ -19,5 +24,41 @@ final class ReceiptSplitViewController: UIViewController {
         super.viewDidLoad()
         
         title = "За что вы хотите заплатить?"
+    }
+    
+    // MARK: - Private
+    
+    private func creatLayout() -> UICollectionViewCompositionalLayout {
+        .init { [weak self] sectionIndex, _ in
+            guard let self = self else { return nil }
+            
+            switch sectionIndex {
+            case 0:
+                return self.createListSection()
+            default:
+                return nil
+            }
+        }
+    }
+    
+    private func createListSection() -> NSCollectionLayoutSection {
+        let cellHeight = CGFloat(64)
+        let item = NSCollectionLayoutItem(
+            layoutSize:.init(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(cellHeight)
+            )
+        )
+        
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: .init(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .absolute(cellHeight)),
+            subitems: [item]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
     }
 }
